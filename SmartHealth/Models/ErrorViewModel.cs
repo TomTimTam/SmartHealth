4e1582e575a58c0416d11d45957cd9f46e8b53f4
TomTimTam <thomaslrcooper@gmail.com>
2019-09-21 13:49:44 +0100

    Initial commit.

---
+++ SmartHealth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SmartHealth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
